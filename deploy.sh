2cb3aed353842101d89b525b5a19ac11709ec3e2
Alexandr Petrenko <alexandr.petrenko.fe@gmail.com>
2021-04-26 16:34:04 +0300

    production version

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 #git push -f git@github.com:Alexandr-Petrenko/Alexandr-Petrenko.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Alexandr-Petrenko/vue-wars.git master:gh-pages
+git push -f git@github.com:Alexandr-Petrenko/vue-characters.git master:gh-pages
 
 cd -
